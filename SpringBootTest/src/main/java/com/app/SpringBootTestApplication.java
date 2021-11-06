59dd2f97cf4a35fedee81a7f4538af0ba0ef3df6
omkarketha <omkarketha@gmail.com>
2021-11-06 13:03:26 +0530

    omr d commit

---
+++ SpringBootTest/src/main/java/com/app/SpringBootTestApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+	public static void main(String[] args) {
+		//gghh
+		SpringApplication.run(SpringBootTestApplication.class, args);
+	}
+
+}
